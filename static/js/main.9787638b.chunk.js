df68ae3dbb4432dd9802dda10630842b15a811f7
Code Sandbox <hello@codesandbox.io>
2021-09-21 04:34:39 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.9787638b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var r=c(1),n=c(3),s=c.n(n),i=(c(8),c(0));function a(){return Object(i.jsxs)("div",{className:"App",children:[Object(i.jsx)("h1",{children:"Hello Nitin Here!"}),Object(i.jsx)("h2",{children:"I am very happy to use react."})]})}var o=document.getElementById("root");s.a.render(Object(i.jsx)(r.StrictMode,{children:Object(i.jsx)(a,{})}),o)},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9787638b.chunk.js.map
